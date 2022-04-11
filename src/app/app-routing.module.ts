17c006204e0620ca6219516e7aa0edc889a3d211
emanSalah12 <eman.salah@roboost.App>
2022-04-11 12:59:10 +0200

    cdk drag drop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CdkDragComponent } from './cdk-drag/cdk-drag.component';
 import { TestDragDropComponent } from './test-drag-drop/test-drag-drop.component';
 
 const routes: Routes = [
 
-  {path:"",component:TestDragDropComponent}
+  {path:"",component:TestDragDropComponent},
+  {path:"cdk",component:CdkDragComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DragulaModule } from 'ng2-dragula';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestDragDropComponent } from './test-drag-drop/test-drag-drop.component';
+import { CdkDragComponent } from './cdk-drag/cdk-drag.component';
 
 @NgModule({
   declarations: [
-    AppComponent,TestDragDropComponent
+    AppComponent,TestDragDropComponent, CdkDragComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, DragulaModule.forRoot()
+    AppRoutingModule, DragulaModule.forRoot(),
+    DragDropModule
 
   ],
   providers: [],
